57ce672469878a3cbdba2755eaad02a2ffa2970b
ilya <ilyagorbachev7@gmail.com>
2020-03-14 13:17:37 +0700

    add main function

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  long int x;
+  long int y;
+} Point;
+
+typedef struct {
+  Point p1;
+  Point p2;
+  Point p3;
+} Triangle;
+
+typedef struct {
+  Point centre;
+  long int radius;
+} Circle;
+
+int main() {
+  Circle circle;
+  char string[100];
+  printf("Input circle and his parametres: \n");
+
+  return 0;
+}
